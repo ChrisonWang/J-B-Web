82f8b86789de4ffd6d4e4dffd566485de8cb4ae4
WytheVan <Wythe110724@hotmail.com>
2017-05-09 10:31:04 +0800

    客户意见修改

--- resources/views/judicial/manage/layout/indexRecommends.blade.php
+++ resources/views/judicial/manage/layout/indexRecommends.blade.php
@@ -11,15 +11,13 @@
                 <thead>
                 <tr>
                     <th class="text-center">标题</th>
-                    <th class="text-center">链接地址</th>
                 </tr>
                 </thead>
                 <tbody class="text-center">
                 @if(is_array($r_list) && count($r_list)>0)
                     @foreach($r_list as $r)
                         <tr>
-                            <td>{{ $r['r_title'] }}</td>
-                            <td><a href="{{ $r['r_link'] }}" target="_blank">{{ $r['r_link'] }}</a></td>
+                            <td><a href="{{ $r['r_link'] }}" target="_blank">{{ $r['r_title'] }}</a></td>
                         </tr>
                     @endforeach
                 @endif
