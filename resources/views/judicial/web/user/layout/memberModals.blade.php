33ba0ca43222b8ffd70ffd9a0872ae7f2ec5f338
WytheVan <Wythe110724@hotmail.com>
2017-05-10 18:06:47 +0800

    个人信息修改UI

--- resources/views/judicial/web/user/layout/memberModals.blade.php
+++ resources/views/judicial/web/user/layout/memberModals.blade.php
@@ -49,20 +49,20 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title text-center" style="font-family: 'MicrosoftYaHei'; font-size: 18px; color: #222222; letter-spacing: 0;">修改账号密码</h4>
+                <h4 class="modal-title text-center" style="font-family: 'MicrosoftYaHei'; font-size: 18px; color: #222222; letter-spacing: 0;">修改个人信息</h4>
             </div>
             <div class="modal-body">
                 <form class="form-horizontal" id="changeInfoForm">
                     <div class="form-group">
                         <label for="oldPassword" class="col-md-4 control-label">您的姓名：</label>
                         <div class="col-md-6">
-                            <input type="text" class="form-control" id="citizen_name" name="citizen_name" placeholder="请输入您的姓名">
+                            <input type="text" class="form-control" id="citizen_name" name="citizen_name" placeholder="请输入您的真实姓名">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="text" class="col-md-4 control-label">您的邮箱：</label>
                         <div class="col-md-6">
-                            <input type="text" class="form-control" id="email" name="email" placeholder="请输入你的邮箱">
+                            <input type="text" class="form-control" id="email" name="email" placeholder="请输入你的邮箱地址">
                         </div>
                     </div>
                     <div class="form-group">
@@ -73,7 +73,7 @@
                     <div class="form-group">
                         <hr/>
                         <div class="col-md-offset-2 col-md-8">
-                            <button type="button" class="btn btn-danger btn-block" onclick="toChangeInfo()">确认</button>
+                            <button type="button" class="btn btn-danger btn-block" onmouseover="javascript: $(this).css('background', '#D22626')" onmouseout="javascript: $(this).css('background', '#d43f3a')" onclick="toChangeInfo()">确认</button>
                         </div>
                     </div>
                 </form>
